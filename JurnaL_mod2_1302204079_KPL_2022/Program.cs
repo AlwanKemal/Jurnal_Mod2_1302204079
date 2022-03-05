e0c7bcccf2773d203c6333e85c2e2cacc8657473
Kemaru345 <alwankemal@student.telkomuniversity.ac.id>
2022-03-05 14:50:33 +0700

    JurnaL_mod2_1302204079_KPL_2022

---
+++ JurnaL_mod2_1302204079_KPL_2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JurnaL_mod2_1302204079_KPL_2022
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
